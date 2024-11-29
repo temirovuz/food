99f4f780cd20c9e3b9af63e16c43e551ee04dd42
temirovuz <temirov-muhammad@mail.ru>
2024-11-29 10:14:07 +0500

    feat(models: Order): order tebleda yangi columlar qoshildi

--- fastfood/models.py
+++ fastfood/models.py
@@ -97,4 +97,19 @@ class OrderItem(Base):
 
 class Order(Base):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order = models.ForeignKey
+    order = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(max_length=255, choices=[
+        ('jarayonda', 'Jarayonda'),
+        ('qabul qilindi', 'Qabul qilindi'),
+        ('tayyor', 'Tayyor'),
+        ('bekor qilindi', 'Bekor qilindi'),
+    ],
+                              default='jarayonda')
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    def __str__(self):
+        return self.status
