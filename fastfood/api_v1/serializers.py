3d0df521c4c8d57a7aec6b793029ffc0fcbf9426
temirovuz <temirov-muhammad@mail.ru>
2024-11-30 21:02:07 +0500

    add(function), update serializers, update admin files

--- fastfood/api_v1/serializers.py
+++ fastfood/api_v1/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework.serializers import ModelSerializer
+
+from fastfood.function import distance, get_user_order_items_quantity, to_divide_into_a_unit_of_time
 from fastfood.models import User, Food, OrderItem, Order
 
 
@@ -30,15 +32,24 @@ class OrderSerializer(ModelSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
+        user = instance.user
+        user_lat = getattr(user, 'latitude', None)
+        user_lon = getattr(user, 'longitude', None)
+
         if instance.status == 'qabul qilindi':
+            preparation_time = get_user_order_items_quantity(user)
             return {
                 'price': instance.price,
                 'status': instance.status,
-                'preparation_time': "test"
+                'quantity': f"{preparation_time} ta buyurtma",
+                'preparation_time': to_divide_into_a_unit_of_time(preparation_time * 2)
             }
         elif instance.status == 'tayyor':
+            distance_to_client = distance(user_lat, user_lon)
+            arrival_time = float(f"{distance_to_client:.2f}")
             return {
-                "arrival time": 'test'
+                "distance": f"{distance_to_client:.2f} km",
+                "arrival time": to_divide_into_a_unit_of_time(arrival_time * 3)
             }
         else:
             return representation
